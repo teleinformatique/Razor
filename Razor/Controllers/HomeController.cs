8fe54477249b5950f2a42a0481937b418672e499
Khadim Sarr <khadimsarr@outlook.com>
2017-12-29 10:35:15 +0000

    creating the view and controller

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Categorie = "Watersport",
+                Price = 275M
+            };
+
+            return View(myProduct);
+        }
+    }
+}
